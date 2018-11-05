77026a69c631c4f1a13954f43a047b911dc5c1ac
Zhabskyi <poltavagaz@gmail.com>
2018-11-04 23:31:32 -0700

    changed

--- src/personal_2/personal_2.js
+++ src/personal_2/personal_2.js
@@ -2,6 +2,6 @@ import './personal_2.scss';
 
 import { SmartLighter } from './scripts/smart-lighter';
 
-colors = ['red', 'yellow', 'green'];
+const colors = ['red', 'yellow', 'green'];
 
 const lighter = new SmartLighter(document.querySelector('.content'), colors);
--- src/personal_2/scripts/smart-lighter.js
+++ src/personal_2/scripts/smart-lighter.js
@@ -4,6 +4,8 @@ export class SmartLighter {
 		this.rootElement = rootElement;
 		this.colors = colors;
 		this.renderWidget();
+		this.renderLighter();
+		this.turnOn();
 	}
 
 	renderWidget() {
@@ -20,9 +22,40 @@ export class SmartLighter {
 	}
 
 	renderLighter() {
+		this.item = [];
 		for (let i = 0; i < this.colors.length; i++) {
-			let item = document.createElement('button');
-			
+			let light = document.createElement('button');
+			light.classList.add('traffic-lighter__lamp');
+			light.classList.add(`traffic-lighter__lamp_${this.colors[i]}`);
+			this.item.push(light);
+			this.lighterContainer.appendChild(light);
 		}
 	}
+
+	tornOnLight(el) {
+		const ACTIVE_CLASS_NAME = 'traffic-lighter__lamp_active';
+		this.activeElement;
+
+			if (this.activeElement !== undefined) {
+				this.activeElement.classList.remove(ACTIVE_CLASS_NAME);
+			}
+			this.activeElement = el;
+			el.classList.toggle(ACTIVE_CLASS_NAME);
+		}
+
+	changeLight() {
+		for (let i = 0; i < this.item.length; i++) {
+			this.item[i].addEventListener('click', () => {
+				this.tornOnLight(this.item[i]);
+			});
+		}
+	}
+
+	turnOn() {
+		this.btn.addEventListener('click', () => {
+			this.btn.classList.toggle('toggle-button_active');
+			this.changeLight();
+		})
+	}
+
 }
