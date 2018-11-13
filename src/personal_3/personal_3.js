16630229a9fb99c83a3d9cd856b559cbe50f59f9
Zhabskyi <poltavagaz@gmail.com>
2018-11-12 22:28:28 -0700

    changed

--- src/personal_3/personal_3.js
+++ src/personal_3/personal_3.js
@@ -2,13 +2,18 @@ import './personal_3.scss';
 
 import { mySlider } from './scripts/my-slider';
 
-const mySlider = new mySlider([
+
+const slidesInfo = [
 	{
 		title: "Hello world",
-		imgUrl: 'http://my-cool-pic.com/',
+		imgUrl: 'https://www.pexels.com/photo/scenic-photography-of-trees-678640/',
 	},
 	{
 		title: "<h2>Hello world</h2>",
-		imgUrl: 'http://my-cool-pic.com/',
+		imgUrl: 'https://www.pexels.com/photo/photo-of-man-sitting-on-hood-1571783/',
 	},
-], 2500);
+];
+
+const timeInterval = 2500;
+
+const mySlider = new mySlider(document.querySelector('.slider'), slidesInfo, timeInterval);
--- src/personal_3/scripts/my-slider.js
+++ src/personal_3/scripts/my-slider.js
@@ -1,5 +1,21 @@
 export class mySlider {
-	constructor(){
-		
+	constructor(rootElement, slidesInfo, timeInterval){
+		this.rootElement = rootElement;
+		this.slidesInfo = slidesInfo;
+		this.timeInterval = timeInterval;
+		this.render();
+	}
+
+	render() {
+		this.slideContainer = document.createElement('div');
+		this.image = document.createElement('img');
+		this.slideContainerDescription = document.createElement('div');
+
+		this.slideContainer.classList = 'slide-container';
+		this.slideContainerDescription.classList = 'slide-container__description';
+
+		this.image.src = slidesInfo[0].imgUrl.src;
+
+		this.slideContainerDescription.innerHTML = this.slidesInfo[0].title;
 	}
 }
