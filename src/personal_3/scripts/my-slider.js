3fea8cf5c30ef8fbe151b894935214a58461f417
Zhabskyi <poltavagaz@gmail.com>
2018-11-16 23:32:14 -0700

    changed

--- src/personal_3/scripts/my-slider.js
+++ src/personal_3/scripts/my-slider.js
@@ -1,4 +1,5 @@
 const ACTIVE_CLASS_NAME = 'slide__container_active';
+const ACTIVE_CONTROLS_CLASS_NAME = 'slide__control_btns_active';
 
 export class mySlider {
 	constructor(rootElement, slidesInfo, timeInterval){
@@ -15,10 +16,12 @@ export class mySlider {
 
 	render() {
 		this.slidesArray = [];
+		this.controlBtnArray = [];
 
 		this.slidesInfo.forEach((slideInfo, i) => {
 			this.slideContainer = document.createElement('div');
 			this.slideContainer.classList.add('slide__container');
+			this.slidesArray.push(this.slideContainer);
 
 			this.image = document.createElement('img');
 			this.slideContainerDescription = document.createElement('div');
@@ -27,10 +30,17 @@ export class mySlider {
 			this.slideContainerDescription.classList.add('slide__container__description');
 		
 			this.image.src = slideInfo.imgUrl;
-
 			this.slideContainerDescription.innerHTML = slideInfo.title;
 
-			this.slidesArray.push(this.slideContainer);
+			this.control = document.createElement('button');
+			this.control.classList.add('slide__control_btns');
+			this.control.addEventListener('click', () => {
+				this.currentSlide = i;
+				this.switchSlide();
+				this.intervalSlideStop();
+				this.intervalDelay();
+			})
+			this.controlBtnArray.push(this.control);
 
 			this.rootElement.appendChild(this.slideContainer);
 			this.slideContainer.appendChild(this.image);
@@ -41,40 +51,64 @@ export class mySlider {
 		this.controlContainer = document.createElement('div');
 		this.controlContainer.classList.add('slide__control');
 
+
 		this.BackBtn = document.createElement('button');
 		this.ForwardBtn = document.createElement('button');
 
-		this.BackBtn.classList.add('slider-back-btn');
-		this.ForwardBtn.classList.add('slider-forward-btn');
+		this.BackBtn.classList.add('slide__control-back-btn');
+		this.ForwardBtn.classList.add('slide__control-forward-btn');
 
 		this.BackBtn.textContent = '<';
 		this.ForwardBtn.textContent = '>';
 
-		this.rootElement.appendChild(this.BackBtn);
-		this.rootElement.appendChild(this.ForwardBtn);
+		this.rootElement.appendChild(this.controlContainer);
+		this.controlContainer.appendChild(this.BackBtn);
+		this.controlContainer.appendChild(this.ForwardBtn);
 
+		for (const item of this.controlBtnArray) {
+			this.controlContainer.appendChild(item);
+		}
+		
 		this.defineActiveSlide();		
 	}
 
 	defineActiveSlide() {
 		this.activeElement = this.slidesArray[this.currentSlide];
+		this.activeControlElement = this.controlBtnArray[this.currentSlide];
 		this.activeElement.classList.add(ACTIVE_CLASS_NAME);
-	}
-
-	switchingSlides() {
-		this.currentSlide = (this.currentSlide+1)%(this.slidesArray.length);
-		this.switchSlide();
+		this.activeControlElement.classList.add(ACTIVE_CONTROLS_CLASS_NAME);
 	}
 
 	switchSlide() {
 		if (this.activeElement !== undefined) {
 			this.activeElement.classList.remove(ACTIVE_CLASS_NAME);
 		}
+		if (this.activeControlElement !== undefined) {
+			this.activeControlElement.classList.remove(ACTIVE_CONTROLS_CLASS_NAME);
+		}
+
 		this.defineActiveSlide();
 	}
 
+	switchingSlides() {
+		this.currentSlide = (this.currentSlide+1)%(this.slidesArray.length);
+		this.switchSlide();
+		console.log(this.currentSlide);
+	}
+
 	intervalSlideRun() {
-		this.slide = setInterval(this.switchingSlides.bind(this), this.timeInterval);
+		this.slideInterval = setInterval(this.switchingSlides.bind(this), this.timeInterval);
+	}
+
+	intervalDelay() {
+		this.slideTimeout = setTimeout(() => {this.intervalSlideRun()}, 5000);
+	}
+
+	intervalSlideStop() {
+		clearInterval(this.slideInterval);
+		clearTimeout(this.slideTimeout);
+		this.slideInterval = undefined;
+		this.slideTimeout = undefined;
 	}
 
 	attachEventHandlers() {
